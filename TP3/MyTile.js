96fe47c6973b7059fc7447324522b5dad4451a39
TiagooGomess <tiagogomes2011@live.com.pt>
2020-12-01 11:36:07 +0000

    Improved texture

--- TP3/MyTile.js
+++ TP3/MyTile.js
@@ -7,7 +7,7 @@ class MyTile {
         this.radius = radius;
         this.piece = null;
         this.isPicked = false;
-        this.tile = new MyCylinder(this.scene, this.radius, this.radius, 0.1, 6, 6);
+        this.tile = new MyCylinder(this.scene, this.radius, this.radius, 0.05, 6, 6);
 
 
         this.defaultAp=new CGFappearance(this.scene);
@@ -24,14 +24,14 @@ class MyTile {
 
 
         this.otherAppearance=new CGFappearance(this.scene);
-        this.otherAppearance.setAmbient(0,1,0,1); // Ambient RGB
-        this.otherAppearance.setDiffuse(0,1,0,1); // Diffuse RGB
-        this.otherAppearance.setSpecular(0,1,0,1); // Specular RGB
-        this.otherAppearance.setEmission(0,1,0,1); // Emissive RGB
+        this.otherAppearance.setAmbient(0.5,0.5,0.5,1); // Ambient RGB
+        this.otherAppearance.setDiffuse(0.5,0.5,0.5,1); // Diffuse RGB
+        this.otherAppearance.setSpecular(0.5,0.5,0.5,1); // Specular RGB
+        this.otherAppearance.setEmission(0.5,0.5,0.5,1); // Emissive RGB/ Emissive RGB
         this.otherAppearance.setShininess(10); 
 
         this.texture2 = new CGFtexture(this.scene, "./scenes/images/tiles/green_tile.png");
-        this.otherAppearance.setTexture(this.texture1);
+        this.otherAppearance.setTexture(this.texture2);
         this.otherAppearance.setTextureWrap('REPEAT', 'REPEAT');
         this.otherAppearance.apply();
         
